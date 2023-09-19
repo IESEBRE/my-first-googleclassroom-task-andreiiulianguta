d9cadffea1abdd6eeec1f33efb8abcbb703044b2
andreiiulianguta <andreiiulianguta@iesebre.com>
2023-09-19 13:15:07 +0200

    He afegit el primer projecte Java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        //Vaig a mostrar per la panatlla el missatge Hello word!!
+        //Shortcut del System.out.println --> sout
+        System.out.println("Hello word!!");
+        System.out.println("Segona linia de text");
+        System.out.print("Em dic Andrei");
+        System.out.println("i soc alumne");
+    }
+}
